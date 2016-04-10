d4d4f988e5201e803bd7627c054b5c5ba35cbbb0
Kohei Abe <koh518@gmail.com>
2016-04-10 14:11:47 +0900

    [add] shake and alert

--- shake/ViewController.swift
+++ shake/ViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("view did load!")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionBegan(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.type == UIEventType.Motion && event?.subtype == UIEventSubtype.MotionShake {
+            print("Shake Motion Began :)")
+            SCLAlertView().showInfo("Shaked!! :)", subTitle: "BEST SHAKE EVER!!!")
+        }
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.type == UIEventType.Motion && event?.subtype == UIEventSubtype.MotionShake {
+            print("Shake Motion Ended :(")
+        }
+    }
+    
+    override func motionCancelled(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.type == UIEventType.Motion && event?.subtype == UIEventSubtype.MotionShake {
+            print("Shake Motion Canceled :(")
+        }
     }
 
 
